d4081e22d778cfbfe6a37154ca7d46ca12791589
Ackou Jens <ackoujens@gmail.com>
2016-07-22 15:52:57 +0200

    Cleanup

--- README.md
+++ README.md
@@ -1,41 +1,41 @@
 # html5-realtime-multiplayer
 html5 realtime multiplayer taking place on a single server with multiple players duking it out.
 
-# Notes
-NodeJS (server)
-npm (library installer, comes with nodejs)
-Atom (text editor)
-Google chrome (client)
+# Tools
+* NodeJS (server)
+* npm (library installer, comes with nodejs)
+* Atom (text editor)
+* Google chrome (client)
 
 # Project Folder Structure
-app.js (main entry point, load all other files, init server)
+* app.js (main entry point, load all other files, init server)
 
-package.json (desc of server, libs using)
+* package.json (desc of server, libs using)
 
-node_modules (contains all libs we have, not modded manually)
-  express
-  socket.io
+* node_modules (contains all libs we have, not modded manually)
+  * express
+  * socket.io
 
-server (contains all logic of game)
-  serverFile1.js
-  serverFile2.js
+* server (contains all logic of game)
+  * serverFile1.js
+  * serverFile2.js
 
-client (accessable by the client)
-  js
-    clientFile1.js
-    clientFile2.js
-    sharedFile1.js
-    sharedFile2.js
-  img
-    myImg1.png
-    myImg2.png
-  index.html
+* client (accessable by the client)
+  * js
+    * clientFile1.js
+    * clientFile2.js
+    * sharedFile1.js
+    * sharedFile2.js
+  * img
+    * myImg1.png
+    * myImg2.png
+  * index.html
 
-File Communication (Express)
+* File Communication (Express)
   Client asks server for a file (Ex: playerImg.png)
 <DOMAIN><PORT><FILE>
 
-Package communication (Socket.io)
+* Package communication (Socket.io)
   Client sends data to server (Ex: Input)
   Server sends data to client (Ex: Monster position)
 
--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+/* ****************************************************** #
+#               html5 realtime multiplayer                #
+#                 written by Jens Ackou                   #
+# ******************************************************* */
+
 // EXPRESS
 var express = require('express')
 var app = express();
 var serv = require('http').Server(app);
 
-// Query = nothing
+// query = nothing
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
-// Query = client
+// query = client
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
@@ -15,23 +20,23 @@ console.log('Server initiated');
 
 
 // WEBSOCKETS
-var io = require('socket.io')(serv,{}); // loads file, inits it, returns io object
-io.sockets.on('connection', function(socket){ // if there is a connection, following function will be ran
+var io = require('socket.io')(serv,{});               // loads file -> inits it -> returns io object
+io.sockets.on('connection', function(socket){         // if there is a connection -> following function will be ran
   console.log('Socket connected');
 
-  socket.on('Message to server', function(){ // receiving message and responding
+  socket.on('Message to server', function(){          // receiving message
     console.log('Client message received')
   });
 
-  socket.on('Sending data to server', function(data){ // receiving message and responding
+  socket.on('Sending data to server', function(data){ // receiving message with attribute
     console.log('Data contents ' + data.contents);
   });
 
-  socket.emit('serverMsg', {
+  socket.emit('serverMsg', {                          // sending a message with attribute
     msg:'This is a message from the server'
   });
 
-  socket.on('buttonclick', function(data){
+  socket.on('buttonclick', function(data){            // reveiving message with 2 attributes (handled by btn)
     console.log('Player ' + data.playernum + ' has clicked on the button: ' + data.btntype);
   });
 });
