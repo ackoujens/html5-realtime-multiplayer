389c2582022e95b26a3f5f08a5486e7af12176f1
Ackou Jens <ackoujens@gmail.com>
2016-07-22 17:43:42 +0200

    Separated player from socket

--- app.js
+++ app.js
@@ -8,38 +8,40 @@ var express = require('express')
 var app = express();
 var serv = require('http').Server(app);
 
-// query = nothing
-app.get('/', function(req, res) {
+app.get('/', function(req, res) {                     // query = empty
   res.sendFile(__dirname + '/client/index.html');
-});
-// query = client
+});                                                   // query = "/client"
 app.use('/client', express.static(__dirname + '/client'));
-
-serv.listen(2000);
-console.log('Server initiated');
+serv.listen(2000);                                    // server will be hosted on port 2000
 
 // WEBSOCKETS
-// create list of sockets
-var SOCKET_LIST = {};
+var SOCKET_LIST = {};                                 // create LIST of SOCKETS
+var PLAYER_LIST = {};
+
+// PLAYER
+var Player = function(id){
+  var self = {
+    x:250,
+    y:250,
+    id:id,
+    number:"" + Math.floor(10 * Math.random())
+  }
+  return self;
+}
 
 var io = require('socket.io')(serv,{});               // loads file -> inits it -> returns io object
 io.sockets.on('connection', function(socket){         // if there is a connection -> following function will be ran
-  socket.id = Math.random();                          // create random socket ID
-  socket.x = 0;                                       // assign x and y params to the socket
-  socket.y = 0;
+  socket.id = Math.random();
   SOCKET_LIST[socket.id] = socket;                    // add socket to the SOCKET_LIST
   console.log('Socket ' + socket.id + ' connected');
 
-  socket.on('Message to server', function(){          // receiving message
-    console.log('Client message received')
-  });
+  var player = Player(socket.id);
+  PLAYER_LIST[socket.id] = player;
 
-  socket.on('Sending data to server', function(data){ // receiving message with attribute
-    console.log('Data contents ' + data.contents);
-  });
-
-  socket.emit('serverMsg', {                          // sending a message with attribute
-    msg:'This is a message from the server'
+  // automatically remove disconnecting players
+  socket.on('disconnect', function(socket){
+    delete SOCKET_LIST[socket.id];
+    delete PLAYER_LIST[socket.id];
   });
 
   socket.on('buttonclick', function(data){            // reveiving message with 2 attributes (handled by btn)
@@ -47,15 +49,23 @@ io.sockets.on('connection', function(socket){         // if there is a connectio
   });
 });
 
-// LOOP every 1000ms/25
+// LOOP every 1000ms / 25
 setInterval(function(){
-  for(var i in SOCKET_LIST){
-    var socket = SOCKET_LIST[i];
-    socket.x++;
-    socket.y++;
-    socket.emit('newPosition', {
-      x:socket.x,
-      y:socket.y
+  var pack = [];                      // create a package to send to the client
+  for(var i in PLAYER_LIST){          // for EACH socket in SOCKET_LIST
+    var player = PLAYER_LIST[i];      // fetch Socket i
+    player.x++;                       // increment X of socket i
+    player.y++;                       // increment Y of socket i
+    pack.push({                       // push x & y DATA of EACH socket into the package
+      x:player.x,
+      y:player.y,
+      number:player.number
     });
   }
+
+  for(var i in SOCKET_LIST){          // for EACH socket in SOCKET_LIST
+    var socket = SOCKET_LIST[i];      // fetch Socket i
+    socket.emit('newPositions', pack);// emit new positions of socket i
+  }
+
 }, 1000/25);
